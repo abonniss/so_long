45bd1a23c1fdd7277d4001cd04710fa656f06304
abonniss <abonnisseau@gmail.com>
2022-01-23 19:41:17 +0100

    work of sunday

--- includes/so_long.h
+++ includes/so_long.h
@@ -6,14 +6,31 @@
 /*   By: abonniss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 22:02:13 by abonniss          #+#    #+#             */
-/*   Updated: 2022/01/19 20:40:02 by abonniss         ###   ########.fr       */
+/*   Updated: 2022/01/21 12:32:27 by abonniss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
+# define WINDOW_WIDHT 	600
+# define WINDOW_HEIGHT 	300
+# define MLX_ERROR	 	1
+# define EXIT_FAILURE   -1
+# define EXIT_SUCCES    0
+# define EXTENSION_NAME ".ber"
+# define EXTENSION_LEN  4
+
+
+
 #include "libft.h"
+#include "get_next_line.h"
 #include "../mlx_linux/mlx.h"
+#include <stdio.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
 
 #endif
--- srcs/so_long.c
+++ srcs/so_long.c
@@ -6,17 +6,69 @@
 /*   By: abonniss <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 22:05:53 by abonniss          #+#    #+#             */
-/*   Updated: 2022/01/20 18:36:48 by abonniss         ###   ########.fr       */
+/*   Updated: 2022/01/21 21:53:36 by abonniss         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include  "so_long.h"
 
-int main (void)
+bool check_empty_file(int fd)
 {
-	void *mlx_ptr;
+	int ret;
+	char str[BUFFER_SIZE];
 
-	mlx_ptr = mlx_init();
-	//mlx_destroy_display(mlx_ptr);
-	//free(mlx_ptr);
+	ret = read(fd, str, BUFFER_SIZE);
+	if (ret <= 0)
+		return (false);
+	return (true);
+}
+
+//pourquoi mettre file_name == NULL ? Alors que deja check avant dans main ? 
+bool check_file_extension(const char *file_name)
+{
+	size_t name_len;
+	size_t i;
+
+	if (file_name == NULL || ft_strlen(file_name) <= EXTENSION_LEN)
+		return (false);
+	name_len = ft_strlen(file_name);
+	i = 0;
+	//remplacer par strncmp
+	while (i <= EXTENSION_LEN)
+	{
+		if ((file_name[(name_len - EXTENSION_LEN) + i] != EXTENSION_NAME[i]))
+			return (false);
+		++i;
+	}
+	return (true);
+}
+
+bool file_checker(char **file)
+{
+	int fd;
+	char str_buff[4096];
+	int ret;
+
+	fd = open(file[1], O_RDONLY);
+	if (fd != -1)
+		if (check_file_extension(file[1]) == true && check_empty_file(fd) != false)
+		{	
+			ret = read(fd, &str_buff, 10);
+			printf("Ret : (%d)\n", ret);
+			str_buff[ret] = '\0';
+			printf("Str_buff: (%s)\n", str_buff);
+			//return (true);
+		}
+	perror("Error message");
+	
+	// A FAIRE -> check if file is not a directory
+	return (false);
+}
+
+int main (int argc, char **argv)
+{	
+	if (argc != 2 || file_checker(argv) != true)
+	 return (EXIT_FAILURE);
+
+	return (EXIT_SUCCESS);
 }
